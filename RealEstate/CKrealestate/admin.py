7e40247477c8cad2e1a26eb3230e4e9041017b9f
msmarywill <65568995+msmarywill@users.noreply.github.com>
2024-04-03 14:37:56 -0500

    Commit tables and Admin models

--- RealEstate/CKrealestate/admin.py
+++ RealEstate/CKrealestate/admin.py
@@ -1,3 +1,26 @@
+from .models import Property_Status, Property_Type, Property_Neighborhood, Property_Price_Range
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Property_Status)
+class Property_StatusAdmin(admin.ModelAdmin):
+    list_display = ('property_status_id', 'property_status_name', )
+    search_fields = ('property_status_name', )
+
+
+@admin.register(Property_Type)
+class Property_TypeAdmin(admin.ModelAdmin):
+    list_display = ('property_type_id', 'property_type_name', )
+    search_fields = ('property_type_name', )
+
+
+@admin.register(Property_Neighborhood)
+class Property_NeighborhoodAdmin(admin.ModelAdmin):
+    list_display = ('neighborhood_id', 'neighborhood_name', )
+    search_fields =('neighborhood_name', )
+
+
+@admin.register(Property_Price_Range)
+class Property_Price_RangeAdmin(admin.ModelAdmin):
+    list_display = ('price_range_id', 'price_range_value', )
+    search_fields = ('price_range_value', )
--- RealEstate/CKrealestate/models.py
+++ RealEstate/CKrealestate/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Property_Type(models.Model):
+    property_type_id = models.AutoField(primary_key=True)
+    property_type_name = models.CharField(max_length=100)
+
+
+class Property_Neighborhood(models.Model):
+    neighborhood_id = models.AutoField(primary_key=True)
+    neighborhood_name = models.CharField(max_length=100)
+
+
+class Property_Status(models.Model):
+    property_status_id = models.AutoField(primary_key=True)
+    property_status_name = models.CharField(max_length=100)
+
+
+class Property_Price_Range(models.Model):
+    price_range_id = models.AutoField(primary_key=True)
+    price_range_value = models.CharField(max_length=100)
