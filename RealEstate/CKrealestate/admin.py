f7f7781520f64e5afb5559830fc4bf7b50cf35a6
msmarywill <65568995+msmarywill@users.noreply.github.com>
2024-04-04 22:20:28 -0500

    Added All tables and Admin area setup, also added Pillow to the requirements file for images

--- RealEstate/CKrealestate/admin.py
+++ RealEstate/CKrealestate/admin.py
@@ -1,4 +1,4 @@
-from .models import Property_Status, Property_Type, Property_Neighborhood, Property_Price_Range
+from .models import Property_Status, Property_Type, Property_Neighborhood, Property_Price_Range, Property, Contact, Property_Photo
 from django.contrib import admin
 
 
@@ -17,10 +17,33 @@ class Property_TypeAdmin(admin.ModelAdmin):
 @admin.register(Property_Neighborhood)
 class Property_NeighborhoodAdmin(admin.ModelAdmin):
     list_display = ('neighborhood_id', 'neighborhood_name', )
-    search_fields =('neighborhood_name', )
+    search_fields = ('neighborhood_name', )
 
 
 @admin.register(Property_Price_Range)
 class Property_Price_RangeAdmin(admin.ModelAdmin):
     list_display = ('price_range_id', 'price_range_value', )
     search_fields = ('price_range_value', )
+
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ('property_id', 'home_type', 'neighborhood', 'status', 'price_range', 'property_street_address',
+                    'property_city', 'property_state', 'property_zip_code', 'property_price', 'property_description',
+                    'property_title', 'property_area', 'property_year_built', 'property_bedroom_count',
+                    'property_bathroom_count', 'property_featured', 'property_active', )
+    search_fields = ('property_id', 'property_title', )
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('contact_id', 'contact_first_name', 'contact_last_name', 'contact_email', 'contact_phone',
+                    'contact_office_street_address', 'contact_office_city', 'contact_office_state', 'contact_office_zip_code',
+                    'contact_website_link', 'contact_profile_description', 'contact_profile_image', )
+
+
+@admin.register(Property_Photo)
+class Property_PhotoAdmin(admin.ModelAdmin):
+    list_display = ('property_photo_id', 'property_photo', 'property')
+    search_fields = ('property_photo_id', 'property', )
+
--- RealEstate/CKrealestate/models.py
+++ RealEstate/CKrealestate/models.py
@@ -1,21 +1,113 @@
 from django.db import models
+import datetime
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Property_Type(models.Model):
-    property_type_id = models.AutoField(primary_key=True)
+    property_type_id = models.AutoField(primary_key=True, unique=True)
     property_type_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.property_type_name
 
 
 class Property_Neighborhood(models.Model):
-    neighborhood_id = models.AutoField(primary_key=True)
+    neighborhood_id = models.AutoField(primary_key=True, unique=True)
     neighborhood_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.neighborhood_name
 
 
 class Property_Status(models.Model):
     property_status_id = models.AutoField(primary_key=True)
     property_status_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.property_status_name
+
 
 class Property_Price_Range(models.Model):
-    price_range_id = models.AutoField(primary_key=True)
+    price_range_id = models.AutoField(primary_key=True, unique=True)
     price_range_value = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.price_range_value
+
+
+def current_year():
+    return datetime.date.today().year
+
+
+def max_value_current_year(value):
+    return MaxValueValidator(current_year())(value)
+
+
+class Property(models.Model):
+    property_id = models.AutoField(primary_key=True, unique=True)
+    home_type = models.ForeignKey(Property_Type, on_delete=models.CASCADE)
+    neighborhood = models.ForeignKey(Property_Neighborhood, on_delete=models.CASCADE)
+    status = models.ForeignKey(Property_Status, on_delete=models.CASCADE)
+    price_range = models.ForeignKey(Property_Price_Range, on_delete=models.CASCADE)
+    property_street_address = models.CharField(max_length=100)
+    property_city = models.CharField(max_length=100)
+    property_state = models.CharField(max_length=2)
+    property_zip_code = models.CharField(max_length=10, default='00000')
+    property_price = models.DecimalField(max_digits=10, decimal_places=2)
+    property_description = models.TextField(max_length=300)
+    property_title = models.CharField(max_length=100)
+    property_area = models.PositiveIntegerField(max_length=9)
+    property_year_built = models.PositiveIntegerField(
+        default=current_year(), validators=[MinValueValidator(1900), max_value_current_year])
+    property_bedroom_count = models.PositiveIntegerField(max_length=2)
+    property_bathroom_count = models.PositiveIntegerField(max_length=2)
+    property_featured = models.BooleanField(default=False)
+    property_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.property_id
+
+
+class Property_Photo(models.Model):
+    property_photo_id = models.AutoField(primary_key=True, unique=True)
+    property_photo = models.ImageField(upload_to=None)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+
+
+class Contact(models.Model):
+    contact_id = models.AutoField(primary_key=True, unique=True)
+    contact_first_name = models.CharField(max_length=100)
+    contact_last_name = models.CharField(max_length=100)
+    contact_phone = models.CharField(max_length=20,default='(402)000-0000')
+    contact_email = models.EmailField
+    contact_website_link = models.CharField(max_length=200)
+    contact_office_street_address = models.CharField(max_length=100)
+    contact_office_city = models.CharField(max_length=100)
+    contact_office_state = models.CharField(max_length=2)
+    contact_office_zip_code = models.CharField(max_length=10, default='00000')
+    contact_profile_image = models.ImageField(upload_to=None)
+    contact_profile_description = models.TextField(max_length=300)
+
+
+class Search_Log(models.Model):
+    search_log_id = models.AutoField(primary_key=True, unique=True)
+    search_date = models.DateField(auto_now_add=True)
+    search_home_type = models.ForeignKey(Property_Type, on_delete=models.CASCADE)
+    search_neighborhood = models.ForeignKey(Property_Neighborhood, on_delete=models.CASCADE)
+    search_price_range = models.ForeignKey(Property_Price_Range, on_delete=models.CASCADE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
