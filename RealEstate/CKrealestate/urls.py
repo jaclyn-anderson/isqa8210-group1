35282b63545a822ac4eac577ad03801ece4dcb08
msmarywill <65568995+msmarywill@users.noreply.github.com>
2024-04-05 21:13:24 -0500

    Omaha Links Images, Omahalinks page, all-listing page, admin landing page changes, views, urls, css

--- RealEstate/CKrealestate/urls.py
+++ RealEstate/CKrealestate/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import home, profile, siteadminlanding
+from .views import home, profile, siteadminlanding, omahalinks, all_listings
 
 urlpatterns = [
     path('', home, name='home'),
     path('profile', profile, name='profile'),
     path('siteadminlanding', siteadminlanding, name='siteadminlanding'),
+    path('omahalinks', omahalinks, name='omahalinks'),
+    path('all_listings', all_listings, name='all_listings'),
 
 ]
--- RealEstate/CKrealestate/views.py
+++ RealEstate/CKrealestate/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Property
 from django.contrib.auth.decorators import login_required
 
 
@@ -6,11 +7,25 @@ def home(request):
     return render(request, 'home.html')
 
 
-@login_required(login_url='home')
-def siteadminlanding(request):
-    return render(request, 'siteadminlanding.html')
+def all_listings(request):
+    property = Property.objects.all()
+
+    return render(request, 'all-listings.html', {'property': property})
 
 
 def profile(request):
     return render(request, 'profile.html')
-    
+
+
+def omahalinks(request):
+    return render(request, 'omahalinks.html')
+
+
+@login_required
+def siteadminlanding(request):
+    property = Property.objects.all()  # for all the records
+
+    return render(request, 'siteadminlanding.html', {'property': property})
+
+
+
