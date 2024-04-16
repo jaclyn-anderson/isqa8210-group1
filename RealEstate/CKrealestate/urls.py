9ff687d90f22788be825fbfbbfab0a99cf98c57c
jaclyn-anderson <janderson174@unomaha.edu>
2024-04-16 11:21:18 -0500

    Started featured property - added views and urls.py
    Found an error on share property page, and began the featured property page, added the views.py and urls.py

--- RealEstate/CKrealestate/urls.py
+++ RealEstate/CKrealestate/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, profile, siteadminlanding, omahalinks, all_listings, add_property, property_details, share_property
+from .views import home, profile, siteadminlanding, omahalinks, all_listings, add_property, property_details, featured, share_property
 
 urlpatterns = [
     path('', home, name='home'),
@@ -9,5 +9,6 @@ urlpatterns = [
     path('all_listings', all_listings, name='all_listings'),
     path('add_property', add_property, name='add_property'),
     path('property_details', property_details, name='property_details'),
+    path('featured', featured, name='featured'),
     path('share_property', share_property, name='share_property'),
 ]
