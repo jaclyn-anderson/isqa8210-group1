bc783fece468144507855d35ae37235ccc6faf46
msmarywill <65568995+msmarywill@users.noreply.github.com>
2024-04-19 08:26:23 -0500

    Made changes for update profile page to only update

--- RealEstate/CKrealestate/views.py
+++ RealEstate/CKrealestate/views.py
@@ -124,14 +124,15 @@ def add_property(request):
 
 @login_required
 def update_profile(request):
+    profile = Contact.objects.first()
     if request.method == 'POST':
-        form = ProfileForm(request.POST, request.FILES)
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
 
         if form.is_valid():
             form.save()
             return redirect('siteadminlanding')
     else:
-        form = ProfileForm()
+        form = ProfileForm(instance=profile)
 
     return render(request, 'update-profile.html', {'form': form})
 
